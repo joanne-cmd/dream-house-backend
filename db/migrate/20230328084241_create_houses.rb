01cf322138b6bd8d5c73dbf63593b59b08ae735a
Teresa <tenjerim@gmail.com>
2023-03-28 12:23:20 +0300

    tables

--- db/migrate/20230328084241_create_houses.rb
+++ db/migrate/20230328084241_create_houses.rb
@@ -2,6 +2,12 @@ class CreateHouses < ActiveRecord::Migration[7.0]
   def change
     create_table :houses do |t|
 
+      t.string :title
+      t.string :location
+      t.string :desc
+      t.integer :price
+      t.string :image
+
       t.timestamps
     end
   end
