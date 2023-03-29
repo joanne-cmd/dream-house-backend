dc0770f64fc8f431938161209258bf312a2425db
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-03-29 05:14:19 +0300

    created the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
         users= User.all
         render json: users
     end 
+    # get a single user with their reviews
+    def show 
+        user= User.find(params[:id])
+        if user
+            render json: user, serializer: UserAndReviewSerializer , status: :ok
+        else
+            render json: {error: "user not found"}, status: :not_found
+        end
+    end
 end
