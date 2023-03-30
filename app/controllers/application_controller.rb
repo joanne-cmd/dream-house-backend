2ec032ed9bde7a7152d2109fe446637e28e3efd5
Teresa <tenjerim@gmail.com>
2023-03-30 23:24:30 +0300

    added post login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,18 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-    private 
+    private
 
+    def authorize
+        @user = User.find_by(id: session[:user_id])
+        
+        if (@user)
+            render @user
+        else
+            render json: {"errors": "Not authorized"}, status: :unauthorized
+        end
+    end
+    
     def render_not_found_response(exception)
         render json: {error: "#{exception.model} not found"}, status: :not_found
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :users
   resources :reviews
   resources :houses
+
+  post "/login", to: "sessions#login"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
